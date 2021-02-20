7b5769ad3a19901344ae3288888316ec14aea739
fatih <70587868+fatcan123@users.noreply.github.com>
2021-02-20 14:27:54 +0300

    ilk commit

---
+++ ödev/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ödev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(26, 13);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 54);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "oyuna_basla";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.oyuna_basla);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(394, 13);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(104, 51);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "oyunu bitir";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.oyunu_bitir);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(254, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 52);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kalan Süre";
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(69, 562);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "*";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(175, 562);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 5;
+            this.button10.Text = "*";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Enabled = false;
+            this.button11.Location = new System.Drawing.Point(267, 562);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 6;
+            this.button11.Text = "*";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Enabled = false;
+            this.button12.Location = new System.Drawing.Point(364, 562);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 7;
+            this.button12.Text = "*";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(566, 562);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 52);
+            this.button13.TabIndex = 8;
+            this.button13.Text = "kelimeyi onayla";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.onayla);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(696, 637);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+    }
+}
+
---
+++ ödev/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace ödev
+{
+    public partial class Form1 : Form
+    {
+        string[] kelimeler = { "FARE", "FİLE", "FAKS" };
+        int tamamlanan = 0;
+        Timer MyTimer;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMyGroupBox();
+            Form1_Load();
+            this.listBox2.Items.AddRange(kelimeler);
+            secilenler.Add(button3);
+            secilenler.Add(button4);
+            secilenler.Add(button5);
+            secilenler.Add(button6);
+            listBox2.SelectedIndex = tamamlanan;
+
+        }
+        int saniye = 60;
+        int kalan = 60;
+        int count = 0;
+        string[] harfler = {"A","B","C","Ç","D","E","F","G","Ğ","H","I","İ","J","K","L","M","N","O","Ö","P","R","S","Ş","T","U","Ü","V","Y","Z"};
+        GroupBox groupBox1;
+        Char[] secilen = new Char[4];
+        string aranan = "";
+        List<Button> secilenler = new List<Button>();
+        List<Button> butonlar = new List<Button>();
+        private void InitializeMyGroupBox()
+        {
+            // Create and initialize a GroupBox and a Button control.
+            groupBox1 = new GroupBox();
+            groupBox1.Location = new Point(20, 70);
+            groupBox1.Size = new Size(400, 350);
+            Button button1 = new Button();
+            button1.Location = new Point(50, 80);
+            button1.Size = new Size(20, 20);
+            Button button2 = new Button();
+            button2.Location = new Point(80, 80);
+            button2.Size = new Size(20, 20);
+            Button button3 = new Button();
+            button3.Location = new Point(110, 80);
+            button3.Size = new Size(20, 20);
+            Button button4 = new Button();
+            button4.Location = new Point(130, 80);
+            button4.Size = new Size(20, 20);
+            Button button5 = new Button();
+            button5.Location = new Point(160, 80);
+            button5.Size = new Size(20, 20);
+            // Set the FlatStyle of the GroupBox.
+            groupBox1.FlatStyle = FlatStyle.Flat;
+
+            butonlar.Add(button1);
+            butonlar.Add(button2);
+            butonlar.Add(button3);
+            butonlar.Add(button4);
+            butonlar.Add(button5);
+
+            foreach (Button btn in butonlar) {
+                groupBox1.Controls.Add(btn);
+                btn.Click += buttonharf_Click;
+            }
+            
+
+            
+
+            
+            // Add the GroupBox to the Form.
+            Controls.Add(groupBox1);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            Random rastgele = new Random();
+
+        }
+        protected void buttonharf_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            Char harf = clickedButton.Text.ToCharArray()[0];
+            aranan = kelimeler[tamamlanan];
+            //secilen.Append("    ");
+
+            int index = -1;
+            if (aranan.Contains(harf)) {
+                 index = aranan.IndexOf(harf);
+            }
+            if (index == 0)
+                {
+                    button9.Text = ""+harf;
+                    secilen[0] = harf;
+                }
+            else if (index == 1)
+            {
+                button10.Text = "" + harf;
+                secilen[1] = harf;
+            }
+            else if (index == 2)
+            {
+                button11.Text = "" + harf;
+                secilen[2] = harf;
+            }
+            else if (index == 3)
+            {
+                button12.Text = "" + harf;
+                secilen[3] = harf;
+            }
+
+            
+            
+        }
+
+
+        private void Form1_Load()
+        {
+            MyTimer = new Timer();
+            MyTimer.Interval = (1 * 1000); // 1sn
+            MyTimer.Tick += new EventHandler(MyTimer_Tick);
+        }
+
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("timer calisti");
+            Random rastgele = new Random();
+            foreach (Button btn in butonlar)
+            {
+                int a = rastgele.Next(0, 28);
+                btn.Text = harfler[a];
+                int x = rastgele.Next(30, 390);
+                int y = rastgele.Next(90, 150);
+
+                btn.Location = new Point(x,y);
+            }
+
+            count++;
+            kalan--;
+
+            label1.Text = "Kalan Süre \n " + kalan;
+
+            if (count == saniye) {
+                MessageBox.Show("ooups zaman doldu.");
+                MyTimer.Stop();
+            }
+            
+        }
+
+        private void onayla(object sender, EventArgs e)
+        {
+            String onaysecilen = new String(secilen);
+            if (aranan == onaysecilen)
+            {
+                MessageBox.Show(tamamlanan + ". kelimeyi tamamladın");
+                tamamlanan++;
+                listBox2.SelectedIndex = tamamlanan;
+            }
+        }
+
+        private void oyuna_basla(object sender, EventArgs e)
+        {
+            MyTimer.Start();
+        }
+
+        private void oyunu_bitir(object sender, EventArgs e)
+        {
+            MyTimer.Stop();
+        }
+    }
+}
